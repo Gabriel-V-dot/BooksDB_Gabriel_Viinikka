5233bcb827a605eeae1dc5d452c94bbd854ab152
Gabriel-V-Dot <gabriel.viinikka@gmail.com>
2025-01-22 20:30:44 +0100

    WIP, ConnectionString set for development, program.cs configured.

---
+++ BooksDB_Gabriel_Viinikka/Models/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksDB_Gabriel_Viinikka.Models
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) :base(options)
+        {
+
+        }
+
+
+
+
+    }
+}
--- BooksDB_Gabriel_Viinikka/Program.cs
+++ BooksDB_Gabriel_Viinikka/Program.cs
@@ -1,3 +1,8 @@
+using BooksDB_Gabriel_Viinikka.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Text.Json.Serialization;
 
 namespace BooksDB_Gabriel_Viinikka
 {
@@ -5,20 +10,36 @@ namespace BooksDB_Gabriel_Viinikka
     {
         public static void Main(string[] args)
         {
+
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "No Environment found";
+            Console.WriteLine($"Environment set to {env.ToUpper()}");
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddControllers().AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+
+            var connectionString = builder.Configuration.GetConnectionString("BooksDb");
+            builder.Services.AddDbContext<LibraryDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString).LogTo(message => Debug.WriteLine(message)).EnableSensitiveDataLogging();
+            });
+
+
+           builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
